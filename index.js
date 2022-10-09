b517c54d29507f7b555b3f010d2ef8fb236f019c
lucasuggeri <lucasuggeri@outlook.com>
2022-10-09 18:23:29 -0300

    observar erro na importação

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectToDatabase from './src/config/database/dbConnector.js';
+
+const confidential = dotenv();
+
+confidential.config();
+
+connectToDatabase();
+
+const app = express();
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
